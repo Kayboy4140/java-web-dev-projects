754924b1f58916a570c5271e927876bdfed374c6
Oluwasegun <kto32@drexel.edu>
2024-04-07 18:07:50 -0400

    studio completed

---
+++ control-flow-and-collections/studio/counting-character/src/main/java/org/launchcode/CharacterCountBonus.java
@@ -0,0 +1,36 @@
+package org.launchcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CharacterCountBonus {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a string to count its character: ");
+        String userString = input.nextLine();
+
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        char[] charactersInString = userString.toUpperCase().toCharArray();
+
+        HashMap<Character, Integer> counts = new HashMap<>();
+
+        for (char letter : charactersInString) {
+            if (alphabet.indexOf(letter) >= 0) {
+                if (counts.containsKey(letter)) {
+                    counts.put(letter, counts.get(letter) + 1);
+                } else {
+                    counts.put(letter, 1);
+                }
+            }
+        }
+
+        for (Map.Entry<Character, Integer> count: counts.entrySet()) {
+            System.out.println(count.getKey() + ": " + count.getValue());
+        }
+    }
+}
