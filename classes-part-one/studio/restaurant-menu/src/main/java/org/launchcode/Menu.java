7c76918817b5b50bdca4c22b3cd723bb75b322d2
Oluwasegun <kto32@drexel.edu>
2024-04-08 23:20:24 -0400

    studio completed

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -0,0 +1,16 @@
+package org.launchcode;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Menu {
+
+    private ArrayList<MenuItem> menuItems = new ArrayList<>();
+    private LocalDate lastUpdated;
+
+    public Menu() {}
+
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+}
